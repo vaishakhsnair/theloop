8fbfb821050e14364cae7235f81910bcdebb76da
Vaishakh S Nair <36193436+vaishakhsnair@users.noreply.github.com>
2023-04-23 14:46:41 +0530

    sendit

--- apiserver/server.py
+++ apiserver/server.py
@@ -28,6 +28,7 @@ def api():
         else:
             content["voltage"].append(request.form['field1'])
             content["percent"].append(request.form['field2'])
+            content["valength"] = [i for i in range(len(request.form['field2']))]
         
         print(content)
 
